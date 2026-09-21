1a1f621a8be1bef247d872d14066be52dba68c46
Bappu Kanti Das <BAPPUDAS70@GMAIL.COM>
2026-09-21 01:16:56 -0600

    added day 1 script

---
+++ scripts/Day1_Rbasics.R
@@ -0,0 +1,33 @@
+# This is a comment
+# Assignment
+A <- 10
+# naming convention
+age <- 20
+num1 <- 30
+# check type of variable
+class(num1)
+help (mean)
+# Text data
+name = "Bappu"
+class(name)
+# logical data (true, False)
+is_weekday <- TRUE
+num2 <- 10
+num3 <-2
+result <- num2*num3
+result <- num2/num3
+result <- num2-num3
+num2 %% 2
+num2^2
+2 == 2
+2 < 1
+5 >=4
+2 != 4
+# logical or
+# logical and
+2 == 2 & 2 > 1
+# logical or
+2 == 2 | 2 < 1
+# logical not
+!2 == 2
+
